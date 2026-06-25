9eff478b3d98bcf5a8d9cd66badfd0baf6760d91
anneliephoto <anneliegayle@gmail.com>
2026-06-25 11:04:08 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'Luxecommerce'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: `/${repoName}/`,
   plugins: [react()],
 })
